b7bcbcc0f620d2c45e6158dfbab5df0281b7d854
singularityliu <604072705@qq.com>
2021-06-30 13:35:43 +0800

    忘记密码

--- auth-server/app/src/main/java/com/neusoft/sl/si/authserver/uaa/controller/interfaces/user/dto/ForgetPwdDTO.java
+++ auth-server/app/src/main/java/com/neusoft/sl/si/authserver/uaa/controller/interfaces/user/dto/ForgetPwdDTO.java
@@ -5,15 +5,13 @@ import lombok.Data;
 
 @Data
 public class ForgetPwdDTO {
-    /** 账户名 */
-    @ApiModelProperty(value = "账号名")
-    private String account;
+    /** 身份证号 */
+    @ApiModelProperty(value = "身份证号")
+    private String idNumber;
     /** 新密码 */
     @ApiModelProperty(value = "新密码")
     private String newPassword;
 
-    @ApiModelProperty(value = "手机号")
-    private String mobile;
 
 
 
